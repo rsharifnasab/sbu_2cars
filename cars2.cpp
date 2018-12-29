4f7ec3ed3b763b1d25bfdd9c99ac0a92f53a4ea4
roozbeh <rsharifnasab@gmail.com>
2018-12-29 17:57:35 +0330

    initial commit

---
+++ cars2.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+int main(int argc, char const *argv[]) {
+  std::cout << "salam \n";
+  return 0;
+}
---
+++ makefile
@@ -0,0 +1,6 @@
+run: compile
+	@./cars2.exe 
+compile: 
+	@g++ cars2.cpp -o cars2.exe -std=c++17
+
+
