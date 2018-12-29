c709b3409c0bbb4f5389ad60883e62e4b10c11fa
roozbeh <rsharifnasab@gmail.com>
2018-12-30 02:33:12 +0330

    added icons to menu

--- cars2.cpp
+++ cars2.cpp
@@ -10,37 +10,96 @@ const unsigned int delay_time = 1000 / FPS;
 // seting up screen and rects size;
 const unsigned int screen_width = 500;
 const unsigned int screen_height = 800;
-const unsigned int divider_width = 6;
-const unsigned int divider_height = screen_height;
 
 
 
-bool load_first_textute()
+bool show_menu_textures()
 {
+
 
 
 }
 
 bool load_menu()
 {
+  const unsigned short int menu_item_size   = 60;
+  bool music_state = true;
+  bool sound_state = true;
+
+
   SBDL::clearRenderScreen();
   Texture menu_background = SBDL::loadTexture( "assets/MenuBackground.png" );
   SBDL::showTexture( menu_background , 0 , 0 );
-	SBDL::updateRenderScreen();
-  //SBDL::delay(10000);
+
+
+  // - - - - write welcome - --
+  Font * welc_font = SBDL::loadFont("assets/Font/gobold.ttf",17);
+  Texture welcome_texture = SBDL::createFontTexture(welc_font , "welcome to 2CARS :)", 100, 240, 33);
+  SBDL::showTexture( welcome_texture , (screen_width - welcome_texture.width ) / 2 ,screen_height/7 );
+
+
+  // - - - - write start - --
+  Font * start_font = SBDL::loadFont("assets/Font/gobold.ttf",25);
+  Texture start_texture = SBDL::createFontTexture(start_font , "press SPACE to start", 20, 140, 133);
+  SBDL::showTexture( start_texture , (screen_width - start_texture.width ) / 2, screen_height/2.2 );
+
+
+  // create and show sound texture
+  Texture sound_texture = SBDL::loadTexture( "assets/Button/Sound.png" );
+  Texture no_sound_texture = SBDL::loadTexture( "assets/Button/SoundDisabled.png" );
+  SBDL::showTexture( sound_texture , 0 , screen_height - menu_item_size );
+  SDL_Rect sound_rect = { 0 , screen_height - menu_item_size , menu_item_size , menu_item_size };
+
+  // create and show music texture
+  Texture music_texture = SBDL::loadTexture( "assets/Button/Music.png" );
+  Texture no_music_texture = SBDL::loadTexture( "assets/Button/MusicDisabled.png" );
+  SBDL::showTexture( music_texture ,(int) 1.5 *  menu_item_size , screen_height - menu_item_size );
+  SDL_Rect music_rect = { (int)1.5 *  menu_item_size , screen_height - menu_item_size , menu_item_size , menu_item_size };
+
+
+  // create and show play texture
+  Texture play_texture = SBDL::loadTexture( "assets/Button/Play.png" );
+  SBDL::showTexture( play_texture ,  screen_width - menu_item_size , screen_height - menu_item_size );
+  SDL_Rect play_rect = { screen_width - menu_item_size , screen_height - menu_item_size , menu_item_size , menu_item_size };
+
+
+
+  SBDL::updateRenderScreen();
+
+
+  while( SBDL::isRunning() ) // handle if start button pressed or not
+  {
+
+    SBDL::updateEvents();
+
+    if (SBDL::keyHeld(SDL_SCANCODE_SPACE)) break;
+    if (SBDL::Mouse.clicked())
+    {
+      if ( SBDL::mouseInRect(play_rect)) music_check(music_state);
+      if ( SBDL::mouseInRect(sound_rect)) sound_check(sound_state);
+    }
+    SBDL::delay(delay_time);
+
+  }
+
+  return true;
 }
 
 bool init()
 {
-  SBDL::InitEngine ("2CARS", screen_width , screen_height);
+  SBDL::InitEngine ("2CARS Game", screen_width , screen_height);
+
+  return true;
 }
 
 
 int main()
 {
-  std::cout << "hello welcometo this game,\n if you are seeing this, maybe sdl problem or it hadnt develped yet \n";
   init();
   load_menu();
+
+  SBDL::updateRenderScreen();
+  //SBDL::delay(10000);
 
   while (true)
   {
--- makefile
+++ makefile
@@ -1,12 +1,13 @@
 
 reset: compile
-	@reset
+	#@reset
 	@./cars2.exe
 run: compile
 	@./cars2.exe
 compile:
 #	@g++ cars2.cpp -o cars2.exe -std=c++17
-	@g++ cars2.cpp -o cars2.exe -std=c++17 -lSDL2 -lSDL2_image -lSDL2_mixer -lSDL2_ttf
+	@reset
+	@g++ cars2.cpp -o cars2.exe -Wall -std=c++17 -lSDL2 -lSDL2_image -lSDL2_mixer -lSDL2_ttf
 clean:
 #	@rm *.exe
 	@rm cars2.exe
