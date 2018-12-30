83b9e5ac9c38f3d444ce5ea859384bb904468004
roozbeh <rsharifnasab@gmail.com>
2018-12-30 13:29:54 +0330

    loaded block texture

--- cars2.cpp
+++ cars2.cpp
@@ -9,9 +9,10 @@ const unsigned int screen_width = 500;
 const unsigned int screen_height = 800;
 const int max_car_height = screen_height * 0.63;
 
+const unsigned double harder = 1.00001;
+unsigned int block_V = 3;
+
 //const int max_car_width = icon_size;
-
-
 
 
 bool music_state = true;
@@ -26,7 +27,6 @@ bool car_l_pos = true;
 
 int car_r_x[] = { screen_width * 5/8  - icon_size/2 , screen_width * 7/8 - icon_size/2 };
 int car_l_x[] = { screen_width * 1/8  - icon_size/2,  screen_width * 3/8 - icon_size/2 };
-
 
 
 
@@ -54,10 +54,16 @@ int main()
   Texture car_r_tex = SBDL::loadTexture( "assets/Cars/BlueCar.png" );
   Texture car_l_tex = SBDL::loadTexture( "assets/Cars/RedCar.png" );
 
+  Texture bad_r_tex = SBDL::loadTexture( "assets/Obstacle/BlueRectangle.png" );
+  Texture bad_l_tex = SBDL::loadTexture( "assets/Obstacle/RedRectangle.png" );
+
+  Texture good_r_tex = SBDL::loadTexture( "assets/Obstacle/BlueCircle.png" );
+  Texture good_l_tex = SBDL::loadTexture( "assets/Obstacle/RedCircle.png" );
+
 
   while( SBDL::isRunning() )
   {
-    unsigned int start_time = SBDL::getTime();
+     unsigned int start_time = SBDL::getTime();
 
     SBDL::updateEvents();
 
@@ -70,14 +76,14 @@ int main()
     }
     if (SBDL::keyHeld(SDL_SCANCODE_Q)) return 0;
 
-
     SBDL::showTexture( background , 0 , 0 );
     SBDL::showTexture( car_r_tex , car_r_x[car_r_pos] , max_car_height );
     SBDL::showTexture( car_l_tex , car_l_x[car_l_pos] , max_car_height );
+
     SBDL::updateRenderScreen();
 
 
-//    show_game_board();
+
     unsigned int loop_time = SBDL::getTime() - start_time;
 		if ( loop_time < delay_time )
 		  SBDL::delay( delay_time - loop_time );
